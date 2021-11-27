9b518432215e2f48a1e47b95027f5ec004c49ee3
Jacob <jmajcan@unb.ca>
2021-11-26 22:04:25 -0400

    Form created

---
+++ src/components/ErrorMessage.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import { Alert } from 'react-bootstrap';
+
+
+class ErrorMessage extends Component {
+
+
+    render() {
+        return (
+            <div>
+                <div class="alert alert-danger" role="alert">
+                    <h4 class="alert-heading">An error occured!</h4>
+                    <p class="mb-0">Error {this.props.status}: {this.props.message}</p>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default ErrorMessage;
--- src/containers/App.js
+++ src/containers/App.js
@@ -1,15 +1,12 @@
 import logo from '../logo.svg';
 import '../style/App.css';
+import TaxForm from '../components/TaxForm';
 
 function App() {
   return (
-    <div className="App">
-      <form>
-        <label>annual income: <input type="number" name="income" /></label><br />
-        <label>tax year: <input type="number" name="income" /></label><br />
-        <input type="submit" value="Submit" />
-      </form>
-    </div>
+    <>
+      <TaxForm />
+    </>
   );
 }
 
